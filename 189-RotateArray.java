f18dea141962814e71ba39d826d6fd6eaaaa6b5e
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-07 02:06:10 +0800

    189. Rotate Array

--- 189-RotateArray.java
+++ 189-RotateArray.java
@@ -1,25 +1,48 @@
-
 // Time: O(n)
-// Space: O(k)
+// Space: O(1)
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
 
-        int[] temp = new int[k];
+        k %= nums.length;
 
-        for (int i = nums.length - k, j = 0; i < nums.length; ++i, ++j) {
-            temp[j] = nums[i];
-        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-        for (int i = nums.length - 1; i >= k; --i) {
-            nums[i] = nums[i - k];
-        }
+    public void reverse(int[] nums, int lo, int hi) {
+        while (lo < hi) {
+            int temp = nums[lo];
+            nums[lo] = nums[hi];
+            nums[hi] = temp;
 
-        for (int i = 0; i < k; ++i) {
-            nums[i] = temp[i];
+            ++lo;
+            --hi;
         }
     }
 }
+
+// Time: O(n)
+// Space: O(k)
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         k = k % nums.length;
+
+//         int[] temp = new int[k];
+
+//         for (int i = nums.length - k, j = 0; i < nums.length; ++i, ++j) {
+//             temp[j] = nums[i];
+//         }
+
+//         for (int i = nums.length - 1; i >= k; --i) {
+//             nums[i] = nums[i - k];
+//         }
+
+//         for (int i = 0; i < k; ++i) {
+//             nums[i] = temp[i];
+//         }
+//     }
+// }
 
 // Time: O(n**k)
 // Space: O(1)
