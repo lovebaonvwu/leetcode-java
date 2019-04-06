9254ecdb70389c30d726afc7481b5ca564267650
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-06 23:19:09 +0800

    58. Length of Last Word

---
+++ 58-LengthOfLastWord.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int ans = 0;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s.charAt(i) != ' ')
+                ++ans;
+            else {
+                if (ans > 0) {
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// use String trim()
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         int ans = 0;
+//         s = s.trim();
+
+//         for (int i = s.length() - 1; i >= 0; --i) {
+//             if (s.charAt(i) != ' ')
+//                 ++ans;
+//             else
+//                 break;
+//         }
+
+//         return ans;
+//     }
+// }
