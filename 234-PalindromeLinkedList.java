1847adce3d97159f21bd47771d95b464131f3428
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-12 09:43:45 +0800

    234. Palindrome Linked List

--- 234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList.java
@@ -7,8 +7,7 @@
  * }
  */
 
-//Time: O(n), Space: O(1)
-class Solution {
+ class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode prev = null;
         ListNode slow = head;
@@ -17,37 +16,74 @@ class Solution {
         if (head == null || head.next == null)
             return true;
 
+        ListNode next;
         while (fast != null && fast.next != null) {
+            // find middle
             fast = fast.next.next;
+
+            // reverse list
+            next = slow.next;
+            slow.next = prev;
             prev = slow;
-            slow = slow.next;
+            slow = next;
+
         }
 
-        prev.next = null;
+        fast = fast == null ? slow : slow.next;
+        slow = prev;
 
-        slow = reverseList(slow);
-
-        while (slow != null && head != null) {
-            if (slow.val != head.val)
+        while (fast != null) {
+            if (fast.val != slow.val)
                 return false;
+            fast = fast.next;
             slow = slow.next;
-            head = head.next;
         }
 
         return true;
     }
-
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode cur = head;
-
-        while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-
-        return prev;
-    }
 }
+
+//Time: O(n), Space: O(1)
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         ListNode prev = null;
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         if (head == null || head.next == null)
+//             return true;
+
+//         while (fast != null && fast.next != null) {
+//             fast = fast.next.next;
+//             prev = slow;
+//             slow = slow.next;
+//         }
+
+//         prev.next = null;
+
+//         slow = reverseList(slow);
+
+//         while (slow != null && head != null) {
+//             if (slow.val != head.val)
+//                 return false;
+//             slow = slow.next;
+//             head = head.next;
+//         }
+
+//         return true;
+//     }
+
+//     private ListNode reverseList(ListNode head) {
+//         ListNode prev = null;
+//         ListNode cur = head;
+
+//         while (cur != null) {
+//             ListNode next = cur.next;
+//             cur.next = prev;
+//             prev = cur;
+//             cur = next;
+//         }
+
+//         return prev;
+//     }
+// }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # leetcode-java
 leetcode java solution
 
-26/1019 Solved - Easy 19 Medium 6 Hard 1
+27/1019 Solved - Easy 20 Medium 6 Hard 1
