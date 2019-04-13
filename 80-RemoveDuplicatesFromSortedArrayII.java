4d56c2e8f7914f60435b5b87c563790304adf694
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-13 18:51:38 +0800

    80. Remove Duplicates from Sorted Array II

--- 80-RemoveDuplicatesFromSortedArrayII.java
+++ 80-RemoveDuplicatesFromSortedArrayII.java
@@ -19,3 +19,21 @@ class Solution {
         return i + 1;
     }
 }
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+
+//         if (nums.length < 2)
+//             return nums.length;
+
+//         int i = 1;
+
+//         for (int j = 2; j < nums.length; ++j) {
+//             if (nums[j] != nums[i - 1]) {
+//                 nums[++i] = nums[j];
+//             }
+//         }
+
+//         return i + 1;
+//     }
+// }
